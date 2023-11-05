9964042958a0f07cc79d1956cde71f6bb6512d86
abcdenis18 <denisfajar24@gmail.com>
2023-11-05 15:37:32 +0700

    Enhance the weather card view, add night/light mode

--- Weather App/ContentView.swift
+++ Weather App/ContentView.swift
@@ -8,43 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .topLeading, 
-                endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            BackgroundView(isNight: isNight)
             
             VStack {
-                Text("Cupertino, CA")
-                    .font(.system(size: 32, weight: .bold, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
+                CityTextView(cityName: "Cupertino, CA")
+                
                 VStack (spacing: 10) {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
+                    Image(systemName: isNight ? "moon.stars.fill" :  "cloud.sun.fill")
+                        .symbolRenderingMode(.multicolor)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 180, height: 180)
                     Text("50°")
-                        .font(.system(size: 70, weight: .medium))
+                        .font(.system(size: 70, weight: .medium ))
                         .foregroundColor(.white)
                 }
-                Spacer()
+                .padding(.bottom, 40)
                 
                 HStack (spacing: 20) {
-                    DailyWeatherInfoCard(dayOfWeek: "Sun", imageName: "cloud.sun.fill", temperature: 50)
+                    DailyWeatherInfoCard(dayOfWeek: "Sun", 
+                                         imageName: "cloud.sun.fill",
+                                         temperature: 50)
+                     
+                    DailyWeatherInfoCard(dayOfWeek: "Mon",
+                                         imageName: "cloud.sun.rain.fill",
+                                         temperature: 22)
                     
-                    DailyWeatherInfoCard(dayOfWeek: "Mon", imageName: "cloud.sun.rain.fill", temperature: 22)
+                    DailyWeatherInfoCard(dayOfWeek: "Tue", 
+                                         imageName: "cloud.sun.bolt.fill",
+                                         temperature: 21)
                     
-                    DailyWeatherInfoCard(dayOfWeek: "Tue", imageName: "cloud.sun.bolt.fill", temperature: 21)
+                    DailyWeatherInfoCard(dayOfWeek: "Wen", 
+                                         imageName: "cloud.rain.fill", 
+                                         temperature: 22)
                     
-                    DailyWeatherInfoCard(dayOfWeek: "Wen", imageName: "cloud.rain.fill", temperature: 22)
-                    
-                    DailyWeatherInfoCard(dayOfWeek: "Thu", imageName: "cloud.bolt.rain.fill", temperature: 23)
-                    
+                    DailyWeatherInfoCard(dayOfWeek: "Thu", 
+                                         imageName: "cloud.bolt.rain.fill",
+                                         temperature: 23)
                 }
                 
                 Spacer()
+                
+                Button {
+                    isNight.toggle()
+                    print("button clicked")
+                } label: {
+                    WeatherButton(label: "Change Day Time", 
+                                  labelColor: Color.blue,
+                                  backgroundColor: Color.white)
+                }
             }
             .padding()
         }
@@ -66,7 +83,7 @@ struct DailyWeatherInfoCard: View {
                 .font(.system(size: 18, weight: .medium))
                 .foregroundColor(.white)
             Image(systemName: imageName)
-                .renderingMode(.original)
+                .symbolRenderingMode(.multicolor)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 50, height: 50)
@@ -74,5 +91,43 @@ struct DailyWeatherInfoCard: View {
                 .font(.system(size: 28, weight: .bold))
                 .foregroundColor(.white)
         }
+    }
+}
+
+struct BackgroundView: View {
+    var isNight: Bool
+    let nightColors: [Color] = [Color.black, Color.gray]
+    let dayColors: [Color] = [Color.blue, Color.lightBlue]
+    
+    var body: some View {
+        ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea()
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .bold, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct WeatherButton: View {
+    var label: String
+    var labelColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(label)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .font(.system(size: 20, weight: .medium, design: .default))
+            .foregroundColor(labelColor)
+            .cornerRadius(10)
     }
 }
