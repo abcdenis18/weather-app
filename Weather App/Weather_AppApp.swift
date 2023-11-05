3d7b28787f62da046dba50402891a000b8e148d0
abcdenis18 <denisfajar24@gmail.com>
2023-11-05 13:14:48 +0700

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Denis Fajar Sidik on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
